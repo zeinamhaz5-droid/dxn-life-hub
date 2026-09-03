8b74e23ec84933db197601759b4f16745761e8cd
zeinamhaz5-droid <322960196+zeinamhaz5-droid@users.noreply.github.com>
2026-09-03 17:55:58 +0300

    Add logging middleware for incoming requests

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.url);
+  next();
+});
 app.use(express.static("."));
 
 const client = new OpenAI({
